03b90ad199fa0ce3d09af1bcabb1bddccc25340b
5335510 <5335510@naver.com>
2022-05-10 14:12:19 +0900

    2022 05-10 14:12

--- src/com/javaex/phonebook/phonebook.java
+++ src/com/javaex/phonebook/phonebook.java
@@ -1,5 +1,6 @@
 package com.javaex.phonebook;
 
+import java.awt.print.PrinterIOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -15,16 +16,10 @@ import java.util.ArrayList;
 public class phonebook {
 
 	public static void main(String[] args) throws IOException {
-	//메뉴
+		Scanner sc = new Scanner(System.in);
 		System.out.println("******************************************");
 		System.out.println("*            전화번호 관리 프로그램            *");
 		System.out.println("******************************************");
-	
-		System.out.println("1.리스트    2.등록    3.삭제    4.검색    5.종료");
-		System.out.println("------------------------------------------");
-		Scanner sc = new Scanner(System.in);
-		System.out.print(">메뉴번호: ");
-		int number = sc.nextInt();
 		
 	//사람리스트
 		List<Person> personList = new ArrayList<Person>();
@@ -38,13 +33,18 @@ public class phonebook {
 			if(str == null) {
 				break;
 			}
-			String[] personInfo = str.split(",");
-			String name = personInfo[0];
-			String hp = personInfo[1];
-			String company = personInfo[2];
-			
-			Person person = new Person(name, hp, company);	//Person클래스에 연결
-			personList.add(person);	//인원을 리스트에 추가
+			try {
+				String[] personInfo = str.split(",");
+				String name = personInfo[0];
+				String hp = personInfo[1];
+				String company = personInfo[2];
+				
+				Person person = new Person(name, hp, company);	//Person클래스에 연결
+				personList.add(person);	//인원을 리스트에 추가
+			}
+			catch(Exception e) {
+				
+			}
 		}
 	
 		
@@ -53,63 +53,88 @@ public class phonebook {
 		
 		
 	//메뉴선택
-		switch (number) {
-		case 1: number = 1;
-				System.out.println("<1.리스트>");
-				int id = 1;
-				for(Person person : personList) {
-					System.out.println(id+".   " + person.getName() +"  "+ person.getHp() +"  "+ person.getCompany());
-					id++;
-				}
-				
+		while(true) {
+			
+			//메뉴			
+		    System.out.println("");
+			System.out.println("1.리스트    2.등록    3.삭제    4.검색    5.종료");
+			System.out.println("------------------------------------------");
+			
+			System.out.print(">메뉴번호: ");
+			int number = sc.nextInt();
+			switch (number) {
+			case 1: number = 1;
+					System.out.println("<1.리스트>");
+					int id = 1;
+					for(Person person : personList) {
+						System.out.println(id+".   " + person.getName() +"  "+ person.getHp() +"  "+ person.getCompany());
+						id++;
+					}
+					
+					break;
+			case 2: number = 2;
+					System.out.println("<2.등록>");
+					System.out.print(">이름: ");
+					String name = sc.next();
+					System.out.print(">휴대전화: ");
+					String hp = sc.next();
+					System.out.print(">회사전화: ");
+					String company = sc.next();
+					System.out.println("[등록되었습니다.]");
+					
+					//사람추가								
+					Person p1 = new Person(name, hp, company);
+					personList.add(p1);
+					break;
+			
+			case 3: number = 3;
+					System.out.println("<3.삭제>");
+					System.out.print(">번호: ");
+					int num = sc.nextInt();
+					for(int i=1; i<=personList.size(); i++) {
+						if(num == i) {
+							personList.remove(i-1);
+						}
+					}												
+					System.out.println("[삭제되었습니다.]");								
+					break;
+			
+			case 4: number = 4;
+					System.out.println("<4.검색>");
+					System.out.print(">이름: ");
+					
+					break;
+			
+			case 5: number = 5;
+					System.out.println("");
+					System.out.println("******************************************");
+					System.out.println("*                 감사합니다                *");
+					System.out.println("******************************************");
+					break;
+			
+			default:
+					System.out.println("[다시 입력해 주세요.]");
+					break;
+			}
+			if(number == 5) {
 				break;
-		case 2: number = 2;
-				System.out.println("<2.등록>");
-				System.out.print(">이름: ");
-				String name = sc.next();
-				System.out.print(">휴대전화: ");
-				String hp = sc.next();
-				System.out.print(">회사전화: ");
-				String company = sc.next();
-				System.out.println("[등록되었습니다.]");
-				
-				//사람추가								
-				Person p1 = new Person(name, hp, company);
-				personList.add(p1);
-				break;
-		case 3: number = 3;
-				System.out.println("삭제");
-				break;
-		case 4: number = 4;
-				System.out.println("검색");
-				break;
-		case 5: number = 5;
-				System.out.println("종료");
-				break;
-		default:
-				System.out.println("[다시 입력해 주세요.]");
-			break;
+			}
+			else {
+				continue;
+			}
 		}
 		
-		
-		
 		//파일 쓰기
-				Writer wr = new FileWriter("C:\\javaStudy\\phonebook\\PhoneDB.txt");
-				BufferedWriter bw = new BufferedWriter(wr); //버퍼를 사용해서 좀 더 효율적으로 사용가능
+			Writer wr = new FileWriter("C:\\javaStudy\\phonebook\\PhoneDB.txt");
+			BufferedWriter bw = new BufferedWriter(wr); //버퍼를 사용해서 좀 더 효율적으로 사용가능
 				
-				for(Person person : personList) {
-					String saveNew = person.getName() + "," + person.getHp() + "," + person.getCompany();
-					//System.out.println(saveNew);
-					bw.write(saveNew); //파일에 새로운 인원 쓰기
-					bw.newLine(); //한줄씩 작성
-				}
-		
-		
-		
-		
-		
-		
-		
+			for(Person person : personList) {
+				String saveNew = person.getName() + "," + person.getHp() + "," + person.getCompany();
+				//System.out.println(saveNew);
+				bw.write(saveNew); //파일에 새로운 인원 쓰기
+				bw.newLine(); //한줄씩 작성
+			}
+			
 		br.close();
 		bw.close();
 		sc.close();
